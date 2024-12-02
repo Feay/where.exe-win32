69e227bc4335e47d24adb3420a510ea8bd08d06d
Feay <fenirstardust@gmail.com>
2024-12-02 07:39:12 -0700

    Add files via upload

---
+++ runwhere.bat
@@ -0,0 +1,7 @@
+echo this is to test both modes of where, interactive makes the console ask
+echo for input where there was non, as though it was called by clicking on where, or by no giving no input to where at the command line
+where
+pause
+echo this tests where is giving output via the console as well, the normal way to use where
+where where
+pause
